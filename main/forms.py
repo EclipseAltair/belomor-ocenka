40badac78709fad5d7b1e2adc91d42d98e7d29f5
EclipseAltair <eclipsealtair@yandex.ru>
2020-03-08 01:22:01 +0300

    add form

---
+++ main/forms.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from .models import Client
+
+
+class ClientForm(forms.ModelForm):
+    name = forms.CharField(max_length=32, required=True, widget=forms.TextInput(attrs={
+        'id': 'name',
+        'class': '',
+        'name': 'name',
+        'placeholder': 'Ваше имя'
+    }))
+    phone = forms.CharField(max_length=16, required=True, widget=forms.TextInput(attrs={
+        'id': 'phone',
+        'class': '',
+        'name': 'phone',
+        'placeholder': '+7(___)___-__-__'
+    }))
+
+    class Meta:
+        model = Client
+        fields = '__all__'
--- main/views.py
+++ main/views.py
@@ -1,9 +1,28 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import JsonResponse
+from .forms import ClientForm
 
 
 def home(request):
-    return render(request, 'main/index.html')
+    form = ClientForm(request.POST or None)
+    return render(request, 'main/index.html', locals())
+
+
+def feedback(request):
+    if request.POST:
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            name = request.POST['name']
+            phone = request.POST['phone']
+            # subject = 'Новый клиент!'
+            # message = 'Имя: ' + name + ' | Номер телефона: ' + phone
+            # sender = 'digitalrushmailer@gmail.com'
+            # recipient = 'belomorocenka@yandex.ru'
+            # send_mail(subject, message, sender, [recipient], fail_silently=False)
+            return JsonResponse({})
 
 
 def error_404(request, exception=None):
