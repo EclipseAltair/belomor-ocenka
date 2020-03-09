a6adb56fdbc5ba657c3e60268fab4dc0b3118ec1
EclipseAltair <eclipsealtair@yandex.ru>
2020-03-09 23:04:49 +0300

    del livereload

--- belomor-ocenka/settings.py
+++ belomor-ocenka/settings.py
@@ -30,7 +30,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'belomor-ocenka.urls'
