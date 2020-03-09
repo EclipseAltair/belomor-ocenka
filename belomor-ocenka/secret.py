f257354b3158f37dd533813fc92f554d0c9d3e5c
EclipseAltair <eclipsealtair@yandex.ru>
2020-03-09 22:41:38 +0300

    del s

--- belomor-ocenka/secret.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-SECRET_KEY = 'am3q^8&+sgow*@)cqip4mfgh78fgh908-546_50$82x9p6)4u'
-
-DB_NAME = 'belomor_ocenka'
-DB_USER = 'belomor_ocenkauser'
-DB_PASSWORD = 'Guido!956'
-
-EMAIL = 'digitalrushmailer@gmail.com'
-EMAIL_PASSWORD = '3ts-SXg-Ndh-dbE'
