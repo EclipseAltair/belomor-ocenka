4973c24a9f7547abb5836ab6ffd703430b3eb5ea
EclipseAltair <eclipsealtair@yandex.ru>
2020-04-22 00:36:39 +0300

    fix apps

--- belomor-ocenka/settings.py
+++ belomor-ocenka/settings.py
@@ -17,9 +17,9 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
     'django.contrib.sitemaps',
     'django.contrib.sites',
+    'django.contrib.staticfiles',
     'main'
 ]
 
