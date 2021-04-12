cceb559ff62caf763bf83b585b26387c822c1482
EclipseAltair <eclipsealtair@yandex.ru>
2021-04-12 21:40:47 +0300

    created_at

--- main/models.py
+++ main/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Client(models.Model):
     name = models.CharField('Имя', max_length=32, blank=False)
     phone = models.CharField('Телефон', max_length=16, blank=False)
+    created_at = models.DateTimeField('Дата заявки', auto_now_add=True, blank=True)
 
     def __str__(self):
         return '{}'.format(self.name)
